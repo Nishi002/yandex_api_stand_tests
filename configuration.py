367f7cf8663c64a923b5c6a5f3ae6cd6265fbad2
Nishi002 <Nishi002@yandex.com>
2023-12-07 21:11:11 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://942eb61f-8462-4df2-a596-fdef80c5fb35.serverhub.praktikum-services.ru"
+LOG_MAIN_PATH = "/docs/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
+
